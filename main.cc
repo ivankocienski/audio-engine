3b441899965bb4fa30ebe5f782379530bec36242
Ivan K <ivan.kocienski@gmail.com>
2015-01-05 10:51:47 +0000

    demo showing off different oscillators

--- main.cc
+++ main.cc
@@ -53,34 +53,34 @@ int main( int arch, char** argv ) {
     }
 
 
-    { // echo things
+    { // volume
       AudioPattern ap;
 
-      // channel, wave, volume, pitch, ms
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.7, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.5, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.2, 523, 50 );
 
-      //BEEP( ap, WF_SINE, 1.0, 1000, 50 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 2500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 2000, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 1500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 1000, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.0, 1000, 100 );
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 0.0, 0, 50 );
 
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.8, 2500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.8, 2000, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.8, 1500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.8, 1000, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.0, 1000, 100 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.2, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.5, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.7, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 523, 50 );
 
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.6, 2500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.6, 2000, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.6, 1500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.6, 1000, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.0, 1000, 100 );
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 0.0, 0, 50 );
 
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.3, 2500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.3, 2000, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.3, 1500, 30 );
-      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.3, 1000, 30 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.7, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.5, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.2, 523, 50 );
+
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 0.0, 0, 50 );
+
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.2, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.5, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 0.7, 523, 50 );
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 523, 50 );
 
       audio.set_pattern( 2, ap );
     }
@@ -110,6 +110,29 @@ int main( int arch, char** argv ) {
       audio.set_pattern( 3, ap );
     }
 
+    {
+      // other thingy
+      AudioPattern ap;
+
+      // channel, wave, volume, pitch, ms
+
+      ap.beep( audio.oscillator(AudioService::WF_SINE), 1.0, 1000, 500 );
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 1.0, 1000, 100 );
+
+      ap.beep( audio.oscillator(AudioService::WF_SQUARE), 1.0, 1000, 500 );
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 1.0, 1000, 100 );
+
+      ap.beep( audio.oscillator(AudioService::WF_NOISE), 1.0, 1000, 500 );
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 1.0, 1000, 100 );
+
+      ap.beep( audio.oscillator(AudioService::WF_SAWTOOTH), 1.0, 1000, 500 );
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 1.0, 1000, 100 );
+
+      ap.beep( audio.oscillator(AudioService::WF_TRIANGLE), 1.0, 1000, 500 );
+      ap.beep( audio.oscillator(AudioService::WF_SILENCE), 1.0, 1000, 100 );
+
+      audio.set_pattern( 4, ap );
+    }
 
     // channel, pattern
     audio.play( 0, 1 );
@@ -120,9 +143,9 @@ int main( int arch, char** argv ) {
 
       window.puts( 10, 10, "Port Audio Demo" );
 
-      if( should_paint(0) ) window.puts( 20, 30, "Sound 1" );
-      if( should_paint(1) ) window.puts( 20, 50, "Sound 2" );
-      if( should_paint(2) ) window.puts( 20, 70, "Sound 3" );
+      if( should_paint(0) ) window.puts( 20, 30, "Sound 1 - volume" );
+      if( should_paint(1) ) window.puts( 20, 50, "Sound 2 - pitch" );
+      if( should_paint(2) ) window.puts( 20, 70, "Sound 3 - waveform" );
       if( should_paint(3) ) window.puts( 20, 90, "Play Loop" );
 
       window.puts( 10, 150, "Press ESCAPE to quit" );
@@ -145,9 +168,9 @@ int main( int arch, char** argv ) {
         case Window::K_ENTER:
 
           switch( selected ) {
-            case 0: audio.play(0, 1); break;
-            case 1: audio.play(0, 2); break;
-            case 2: audio.play(0, 3); break;
+            case 0: audio.play(0, 2); break;
+            case 1: audio.play(0, 3); break;
+            case 2: audio.play(0, 4); break;
 
             case 3: 
               if( audio.channel(1).is_looping() ) 
